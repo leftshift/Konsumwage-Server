5f3fc133119dde24c4d9ab86f16bab93411436bd
Adrian <leftshiftlp@gmail.com>
2017-02-08 23:48:07 +0100

    using propper url scheme

--- main/frontend/static/dashboard.js
+++ main/frontend/static/dashboard.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-  var socket = io.connect(null, {port: 5000, rememberTransport: false});
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
   socket.on('new_values', function (msg) {
+    console.log(new_values);
     $('.measurements tbody').append('<tr><td>'+msg.total+'</td><td>'+msg.last_minute+'</td><td>'+msg.last_delta.consumtion + '/' + msg.last_delta.time+'</td><td>'+msg.average+'</td></tr>');
   });
 });
