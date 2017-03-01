e6a19d075ac43e4cf5fca243e2a24a54ec1a8c12
Adrian Schneider <leftshiftlp@gmail.com>
2017-03-02 00:59:06 +0100

    moved on-the-fly-calculations to the backend

--- main/api/operations.py
+++ main/api/operations.py
@@ -1,8 +1,14 @@
+import datetime
 from flask_socketio import emit
 from threading import Timer, Event
 
 from .. import socketio
-from ..db import operations, deductions
+from ..db import db, models, operations, deductions
+
+
+def minor_update(value):
+    return operations.calulate_minor_update(value)
+
 
 def add_measurement(timestamp, value):
     print("Adding Measurement at %s of %i" % (timestamp, value))
@@ -14,7 +20,8 @@ def add_measurement(timestamp, value):
         major_update_timer = Timer(10.0, set_major_update_flag)
         major_update_timer.start()
     else:
-        socketio.emit('minor_update', deductions.stats())
+        socketio.emit('minor_update', minor_update(value))  # need to factor in factor
+
 
 def set_major_update_flag():
     major_update_event.set()
--- main/db/operations.py
+++ main/db/operations.py
@@ -1,5 +1,40 @@
 import datetime
+from threading import Lock
 from . import db, models
+
+last_measurement_lock = Lock()
+
+last_measurement = {
+    'timestamp': datetime.datetime.now(),
+    'weight': 0,
+    'consumtion': 0,
+}
+
+
+def calculate_consumtion_delta(weight_delta):
+    if weight_delta < 0:  # weight was added
+        consumtion_delta = 0
+    else:
+        consumtion_delta = weight_delta * 1  # TODO: Replace with config value
+    return consumtion_delta
+
+
+def calulate_minor_update(value):
+    with last_measurement_lock:
+        global last_measurement
+        now = datetime.datetime.now()
+        delta_time = now - last_measurement['timestamp']
+        delta_weight = last_measurement['weight'] - value
+        delta_consumtion = calculate_consumtion_delta(delta_weight)
+        total = last_measurement['consumtion'] + delta_consumtion
+
+        last_measurement['timestamp'] = now
+        last_measurement['weight'] = value
+        last_measurement['consumtion'] = total
+    return {
+        'delta_time': delta_time.total_seconds(),
+        'delta_consumtion': delta_consumtion,
+        'total': total}
 
 
 def add_measurement(timestamp, weight):
@@ -8,10 +43,7 @@ def add_measurement(timestamp, weight):
     if prev_meas:
         time_delta = timestamp - prev_meas.timestamp
         weight_delta = prev_meas.weight - weight
-        if weight_delta < 0:  # weight was added
-            consumtion_delta = 0
-        else:
-            consumtion_delta = weight_delta * 1  # TODO: Replace with config value
+        consumtion_delta = calculate_consumtion_delta(weight_delta)
         consumtion = prev_meas.consumtion + consumtion_delta
     else:
         time_delta = datetime.timedelta(0)
@@ -23,3 +55,8 @@ def add_measurement(timestamp, weight):
                               consumtion_delta, weight)
     db.session.add(meas)
     db.session.commit()
+    with last_measurement_lock:
+        global last_measurement
+        last_measurement['timestamp'] = timestamp
+        last_measurement['weight'] = weight
+        last_measurement['consumtion'] = consumtion
